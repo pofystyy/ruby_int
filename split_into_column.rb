d5ba1d0efbd32000239c79f3e86a0aa0178a26ae
pofystyy <pofystyy@gmail.com>
2023-11-16 11:45:50 +0200

    solution for def split_into_column

---
+++ split_into_column.rb
@@ -0,0 +1,42 @@
+ALERT = 'check input data'
+
+def split_into_column(array, columns)
+  return ALERT unless right_data?(array, columns)
+  return array if array.count < columns  
+
+  steps = (array.count / columns.to_f).ceil
+  
+  arr = []
+  steps.times do |i|
+    arr << array.first(columns) 
+    array = array[columns..-1]
+  end
+  # or
+  # arr = array.each_slice(columns).to_a
+
+  count_of_nil = columns - arr.last.count
+  arr.last.concat( [nil] * count_of_nil)
+  
+  arr
+end
+
+def right_data?(array, columns)
+  Array === array && Integer === columns && columns > 0
+end
+
+# a list of data for checking split_into_column()
+test_data = [
+  [[1,2,3,4,5], 2],                # => [[1, 2], [3, 4], [5, nil]]
+  [[1,2,3,4,5], 3],                # => [[1, 2, 3], [4, 5, nil]]
+  [[1,2,3,4,5], 7],                # => [1, 2, 3, 4, 5]
+  [[1,2,3,4,5,6,7,8,9,10], 8],     # => [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10, nil, nil, nil, nil, nil, nil]]
+  [[1,2,3,4,5,6,7,8,9,10], 2],     # => [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+  [[1,2,3,4,5,6,7,8,9,10], 100],   # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  ['hello world', 15],             # => "check input data"
+  [[], 0],                         # => "check input data"
+  [[], 'foo']                      # => "check input data"
+]
+
+test_data.each do |array, columns|
+  p split_into_column(array, columns)
+end
